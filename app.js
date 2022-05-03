569201006f48a36580433024427db4ad53c5325a
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-02 17:58:02 -0700

    worked on displayIngredients function

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+import { renderIngredient } from './utils.js';
+
 const ingredientForm = document.getElementById('ingredients-added');
+const ingredientsList = document.getElementById('ingredients-list');
 
 let ingredientArray = [];
 
@@ -13,9 +16,15 @@ ingredientForm.addEventListener('submit', (e) => {
         unit: ingredientData.get('measurement-type'),
     };
     ingredientArray.push(mealIngredients);
+    displayIngredients();
     ingredientForm.reset();
 });
 
 function displayIngredients() {
-  
+    ingredientsList.textContent = '';
+    for (let ingredient of ingredientArray) {
+        const ingredientList = renderIngredient(ingredient);
+        ingredientList.appendChild(ingredient);
+    }
 } 
+console.log(renderIngredient);
--- utils.js
+++ utils.js
@@ -2,4 +2,5 @@ export function renderIngredient(ingredient) {
     const ingredientList = document.getElementById('ingredient-list'
     );
     ingredientList.textContent = `${ingredient.quantity} ${ingredient.unit} of ${ingredient.ingredients} `;
+    return ingredientList;
 }
