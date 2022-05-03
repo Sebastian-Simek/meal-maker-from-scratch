3417d26259fda25dbc0048bd253e042316f39622
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-02 21:20:25 -0700

    started work on CSS

--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+
 export function renderIngredient(ingredient) {
     const ingredientList = document.createElement('li');
     ingredientList.textContent = `${ingredient.quantity} ${ingredient.unit} of ${ingredient.ingredients}`;
