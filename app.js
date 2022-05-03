2b420978b7ceb61c2dcb13e03c42328c41173406
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-03 14:40:53 -0700

    added savedMeals object

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ import { renderIngredient } from './utils.js';
 const ingredientForm = document.getElementById('ingredients-added');
 const ingredientsList = document.getElementById('ingredient-list');
 const removeButton = document.getElementById('remove');
+const saveButton = document.getElementById('save-meal');
+const mealName = document.getElementById('meal-name');
 
 let ingredientArray = [];
+let savedMealsArray = [];
 
 ingredientForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -31,4 +34,16 @@ function displayIngredients() {
 removeButton.addEventListener('click', () => {
     ingredientArray.pop();
     displayIngredients();
+});
+
+saveButton.addEventListener('click', () => {
+    // for (let numOfIngredients of ingredientArray){
+    //     let ourIngredients = numOfIngredients.Ingredients;
+    //     console.log(ourIngredients);
+    
+    let savedMeals = {
+        Name: mealName.value,
+        Ingredients: ingredientArray.length,
+    };
+    console.log(savedMeals);
 });
